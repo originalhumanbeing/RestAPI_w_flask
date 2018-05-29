edf60aa6e05dc20fc7d1fe4cb9946fbc4aa51efb
originalhumanbeing <han.hyunjung17@gmail.com>
2018-05-29 23:28:22 +0900

    Add index, Implement APIs (create store, read store)

--- app.py
+++ app.py
@@ -1,11 +1,36 @@
-from flask import Flask
+from flask import Flask, jsonify, request, render_template
 
 app = Flask(__name__)
 
 
+stores = [{
+        'name': 'My Store',
+        'items': [{'name': 'my item', 'price': 15.99}]
+    }]
+
+
 @app.route('/')
-def hello_world():
-    return 'Hello World!'
+def home():
+    return render_template('index.html')
+
+#post /store data: {name :}
+@app.route('/store')
+def create_store():
+    request_date = request.get_json()
+    new_store = {
+        'name': request_date['name'],
+        'items': []
+    }
+    stores.append(new_store)
+    return jsonify(new_store)
+
+#get /store/<name> data: {name: }
+@app.route('/store/<string:name>')
+def get_store(name):
+    for store in stores:
+        if store['name'] == name:
+            return jsonify(store)
+    return jsonify({'msg': 'store not found'})
 
 
 if __name__ == '__main__':
