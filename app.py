4a1fccda0aa131619dccdbb9a0e06a7a5a58abd4
originalhumanbeing <han.hyunjung17@gmail.com>
2018-05-30 23:52:23 +0900

    Implement APIs (get stores, create item in them)

--- app.py
+++ app.py
@@ -2,7 +2,6 @@ from flask import Flask, jsonify, request, render_template
 
 app = Flask(__name__)
 
-
 stores = [{
         'name': 'My Store',
         'items': [{'name': 'my item', 'price': 15.99}]
@@ -14,11 +13,11 @@ def home():
     return render_template('index.html')
 
 #post /store data: {name :}
-@app.route('/store')
+@app.route('/store', methods=['POST'])
 def create_store():
-    request_date = request.get_json()
+    request_data = request.get_json()
     new_store = {
-        'name': request_date['name'],
+        'name': request_data['name'],
         'items': []
     }
     stores.append(new_store)
@@ -30,7 +29,23 @@ def get_store(name):
     for store in stores:
         if store['name'] == name:
             return jsonify(store)
-    return jsonify({'msg': 'store not found'})
+    return jsonify({'message': 'store not found'})
+
+#get /store
+@app.route('/store')
+def get_stores():
+    return jsonify({'stores': stores})
+
+#post /store/<name> data: {name :}
+@app.route('/store/<string:name>/item' , methods=['POST'])
+def create_item_in_store(name):
+    request_data = request.get_json()
+    return jsonify(request_data)
+
+#get /store/<name>/item data: {name :}
+@app.route('/store/<string:name>/item')
+def get_item_in_store(name):
+    pass
 
 
 if __name__ == '__main__':
