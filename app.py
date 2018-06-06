7f87770e340e05e30215f42c45adeebe1a9e42e9
originalhumanbeing <han.hyunjung17@gmail.com>
2018-06-06 22:54:20 +0900

    retrieve data using request from flask

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request
 from flask_restful import Resource, Api
 
 app = Flask(__name__)
@@ -15,12 +15,20 @@ class Item(Resource):
         return {'item': None}, 404
 
     def post(self, name):
-        item = {'name': name, 'price': 12.00}
+        data = request.get_json() #get_json result = dict
+        item = {'name': name, 'price': data['price']}
         items.append(item)
         return item, 201
 
 
+class ItemList(Resource):
+    def get(self):
+        return {'items': items}
+
+
 api.add_resource(Item, '/item/<string:name>')
+api.add_resource(ItemList, '/items/')
+
 
 if __name__ == '__main__':
     app.run(debug=True)
